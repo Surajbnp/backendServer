1e7b33ffba72c5748eaee7bbc44c05804d2a9cb4
suraj <surajguptabnp14c@gmail.com>
2023-02-02 09:36:13 +0530

    upload

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ app.use(
   })
 );
 
+app.use(function (req, res, next){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
 app.use(
   cookieSession({
     name: "mysession",
